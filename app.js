6422bb7e0f5fff93c8dbb2a14d3874c40bfe9496
[Ahmet Albayrak] <[ahmetalperenalbayrak@gmail.com]>
2022-01-25 08:03:29 +0300

    Final version of the project

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const newYears ='1st Jan 2021'
+const countdown = () => {
+    // We added getTime method in order to get the time in numbers so we could play around with it //
 
-function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
+    // Kindly note by assigning a new date it should be in the future //
+
+    const countDate = new Date ("May 17,2023 00:00:00").getTime();
+
+    // with gap variable we find the difference between now and the date given //
+
+    const now = new Date().getTime();
+    const gap = countDate - now
+
+    // with these variables we could get timer work
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // calculate the time
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+// We add the results to our relevant places of our HTML 
+    document.querySelector(".day").innerText = textDay
+    document.querySelector(".hour").innerText = textHour
+    document.querySelector(".mins").innerText = textMinute
+    document.querySelector(".second").innerText = textSecond
+
+    // with an if statement we could run aother fuction if the countdown is near to zero
+    // if ( gap < 10000){
+    //     function(); }
 
 }
+
+// with setInterval function we set a page refresher in given millisecond // 
+setInterval(countdown, 1000)
